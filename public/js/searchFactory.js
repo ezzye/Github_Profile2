da00fb2becb5713b4efd753b9477c008240a2052
Ezzy Elliott <ezzy.elliott@gmail.com>
2015-12-03 16:21:59 +0000

    Removed access token

--- public/js/searchFactory.js
+++ public/js/searchFactory.js
@@ -6,8 +6,8 @@ githubUserSearch.factory('Search', ['$http', function($http) {
         url: queryUrl,
         method: 'GET',
         params: {
-          'q': searchTerm,
-          'access_token': '66ffff07ac77d26ee62d9d140cf8820af73a77dd'
+          'q': searchTerm
+          // 'access_token': '66ffff07ac77d26ee62d9d140cf8820af73a77dd'
         }
       });
     }
